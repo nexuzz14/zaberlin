906a04cec79bb483852f330e261f02ec7aad960b
nexx14 <mnabilcf65@gmail.com>
2026-04-30 19:02:58 +0700

    fix: title and description website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="@yield('meta_description', 'Zaberlin TV - Platform streaming video podcast dan edukasi terbaik Indonesia')">
-    <title>@yield('title', 'Zaberlin TV') | Streaming Podcast & Edukasi</title>
+    <meta name="description" content="@yield('meta_description', 'Zaberlin TV - Platform streaming video podcast, edukasi, variety show, dan iklan komersial terbaik Indonesia')">
+    <title>@yield('title', 'Zaberlin TV') | Streaming Podcast, Edukasi & Hiburan</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
