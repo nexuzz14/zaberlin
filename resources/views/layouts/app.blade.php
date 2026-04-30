6c9ea453d2d82965adcb392cd2ce687e7fcb394d
nexx14 <mnabilcf65@gmail.com>
2026-04-30 19:00:46 +0700

    feat: add icon website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Outfit:wght@400;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><defs><linearGradient id='g' x1='0%25' y1='0%25' x2='100%25' y2='100%25'><stop offset='0%25' stop-color='%230E49B5'/><stop offset='100%25' stop-color='%23ED0101'/></linearGradient></defs><rect width='100' height='100' rx='20' fill='url(%23g)'/><path d='M38 28 v44 l36 -22 z' fill='white'/></svg>">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('head')
 </head>
